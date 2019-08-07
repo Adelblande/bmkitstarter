8ee9ecbd66574c259b6392d7ad89b9200b4dcade
Adelblande <adelblande@gmail.com>
2019-08-07 02:00:19 -0300

    Iniciando App

---
+++ BMmobile/src/components/login/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import { View, Text } from 'react-native';
+
+// import { Container } from './styles';
+
+export default class Login extends Component {
+  static navigationOptions = () => ({
+    header: null
+  })
+  render() {
+    return (
+      <View>
+        <Text>Adelblande</Text>
+      </View>
+    );
+  }
+}
---
+++ BMmobile/src/routes.js
@@ -0,0 +1,9 @@
+import { createAppContainer, createStackNavigator } from 'react-navigation';
+
+import Login from './components/login';
+
+export default createAppContainer(
+  createStackNavigator({
+    Login
+  })
+)
